1e524b87115cfb6ece1cb35c836203952780fd48
MohamedMohamady125 <mohamadmohamady125@gmail.com>
2025-07-29 03:54:51 +0300

    7th

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "acae9d5ae1d2bd8e8035a982a6cca5bf",
+const RESOURCES = {"flutter_bootstrap.js": "8693d5ab32eba87be76cd827063ae713",
 "version.json": "39a4fbe1894389a54bf2a20f130c80ad",
 "index.html": "99606990c53064607a0b76f97dcff432",
 "/": "99606990c53064607a0b76f97dcff432",
-"main.dart.js": "fd753012de9e1e8f345db1371f770dd6",
+"main.dart.js": "c2ff59806e77bf0a57217b5607b10e2e",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "sounds/hfasound.mp3": "bf4157343c6829eb2ce86c3b96fd708f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
